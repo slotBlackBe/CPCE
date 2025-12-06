30498896d1ca706ca32f13cb276f68109720b051
Slot <kluirittiphang@gmail.com>
2025-12-06 23:35:54 +0700

    Lab2.3

---
+++ Lab2/Lab2.3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void explode(char str1[], char splitter, char str2[][10], int *count) {
+    int i = 0, j = 0, k = 0;
+    while (str1[i] != '\0') {
+        if (str1[i] == splitter) {
+            str2[k][j] = '\0';
+            k++;
+            j = 0;
+        } else {
+            str2[k][j++] = str1[i];
+        }
+        i++;
+    }
+    str2[k][j] = '\0';
+    *count = k + 1;
+}
+
+int main() {
+    char out[20][10];
+    int num;
+    explode("I/Love/You", '/', out, &num);
+
+    printf("--- EXPLODED STRINGS ---\n");
+    for (int i = 0; i < num; i++) {
+        printf("[%d] %s\n", i, out[i]);
+    }
+
+    return 0;
+}
