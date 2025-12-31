5a58573424ec8e691276a82cc77374663149d124
Slot <kluirittiphang@gmail.com>
2025-12-31 15:49:15 +0700

    Lab4.2

---
+++ Lab4/Lab4.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void go(int **p, int *z) {
+    *p = z;
+}
+
+int main() {
+    int *a;      
+    int b = 10;
+    int c = 20;
+
+    go(&a, &b);
+    printf("%d %p %p\n", *a, a, &a);
+
+    go(&a, &c);
+    printf("%d %p %p\n", *a, a, &a);
+
+    return 0;
+}
