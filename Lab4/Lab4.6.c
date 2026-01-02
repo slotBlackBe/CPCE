b24f9818d29a5ddf84c836c0956b3a755910318a
Slot <kluirittiphang@gmail.com>
2026-01-02 13:06:17 +0700

    Lab4.6

---
+++ Lab4/Lab4.6.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct studentNode {
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+    struct studentNode *next;
+};
+
+void SaveNode(struct studentNode *child,
+              char n[], int a, char s, float g) {
+    strcpy(child->name, n);
+    child->age = a;
+    child->sex = s;
+    child->gpa = g;
+    child->next = NULL;
+}
+
+void GoNext2(struct studentNode ***walk) {
+    if ((**walk)->next != NULL) {
+        **walk = (**walk)->next;  
+        printf("GoNext2 -> %s %d %c %.2f\n",
+               (**walk)->name,
+               (**walk)->age,
+               (**walk)->sex,
+               (**walk)->gpa);
+    } else {
+        printf("No next node\n");
+    }
+}
+
+int main() {
+    struct studentNode *start, *now1;
+    struct studentNode **now2;
+
+    start = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start, "one", 6, 'M', 3.11);
+
+    start->next = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next, "two", 8, 'F', 3.22);
+
+    start->next->next = (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next->next, "three", 10, 'M', 3.33);
+
+    start->next->next->next =
+        (struct studentNode *)malloc(sizeof(struct studentNode));
+    SaveNode(start->next->next->next, "four", 12, 'F', 3.44);
+
+    now1 = start;
+    now2 = &start;
+
+    GoNext2(&now2); 
+
+    printf("After GoNext2, now2 = %s\n", (*now2)->name);
+
+    return 0;
+}
